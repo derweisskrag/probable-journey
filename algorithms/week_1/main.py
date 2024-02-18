9511cd823d1b238b8eb1701218324845c07d8474
derweisskrag <kompasj@mail.ru>
2024-02-18 10:04:59 +0000

    Nice

--- algorithms/week_1/main.py
+++ algorithms/week_1/main.py
@@ -1,8 +1,11 @@
 """Entry point"""
 
+from implementation.linked_list import LinkedList, Node
+
 def main():
     """Main function"""
-    print("Nothing")
+    linked_list = LinkedList(Node(1))
+    print(linked_list.has_head())
 
 if __name__ == "__main__":
     main()
